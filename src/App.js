979589c175132c94e7ba8723bc0c2be82e51bdb3
Abraham <abrahanrivero9@gmail.com>
2021-02-16 00:00:51 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import  Mensaje from  './mensaje.js';
 
-function App() {
+
+const Description = () => {
+
   return (
+
+  <p>
+    Esta es la descripcion del la <strong>Pagina</strong>
+  </p>
+
+  )
+}
+
+
+
+const App = ()=> {
+  return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Mensaje color="red" message="Estamos trabajando" />
+    <Mensaje color="green" message="En un Curso" />
+    <Mensaje color="yellow" message="React" />
+    <Description />
+
     </div>
+    
+
   );
 }
 
---
+++ src/mensaje.js
@@ -0,0 +1,18 @@
+const Mensaje = (props) =>{
+
+	return (
+
+	<h1 style={{color: props.color}}>
+			
+			{props.message}
+	
+
+	</h1>
+
+	)
+}
+
+//con props paramos los parametros a message
+
+
+export default Mensaje;
